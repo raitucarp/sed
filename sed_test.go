14849ae432ef499e363263d92fc3843ce3512107
raitucarp <ribhararnus.pracutiar@gmail.com>
2015-05-25 14:36:28 +0700

    update test

--- sed_test.go
+++ sed_test.go
@@ -4,19 +4,20 @@ import "testing"
 
 func TestEncryptDecrypt(t *testing.T) {
 	cases := []struct {
+		output int
 		key, original, deciphered string
 	}{
 		// use base 32
-		{"I am the key", "Hello, world", "Hello, world"},
+		{BASE32, "I am the key", "Hello, world", "Hello, world"},
 		// use base 64
-		{"Is this works?", "Hello, 世界", "Hello, 世界"},
+		{BASE64, "Is this works?", "Hello, 世界", "Hello, 世界"},
 		// hex
-		{"what is your name?", "Hello, world", "Hello, world"},
+		{HEX, "what is your name?", "Hello, world", "Hello, world"},
 	}
 
 	for _, text := range cases {
-		e := Encrypt(text.original, text.key, BASE32)
-		d := Decrypt(e, text.key, BASE32)
+		e := Encrypt(text.original, text.key, text.output)
+		d := Decrypt(e, text.key, text.output)
 		if text.original != d {
 			t.Errorf("%s != %s using key %s", text.original, d, `"text.key"`)
 			t.Errorf("the ciphertext is", e)
